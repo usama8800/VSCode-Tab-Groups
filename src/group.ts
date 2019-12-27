8dff418a8d9ca1d2ef154a544918667b65b3bec7
Usama Ahsan <usama8800@gmail.com>
2019-12-27 15:49:24 +0500

    Save base64 instead of multiline thing

--- src/group.ts
+++ src/group.ts
@@ -9,19 +9,22 @@ export class Groups {
     groups: Group[];
 
     constructor() {
-        this.groups = vscode.workspace.getConfiguration().get('tab-groups.groups', []);
+        const base64 = vscode.workspace.getConfiguration().get('tab-groups.groups', "");
+        this.groups = JSON.parse(Buffer.from(base64, 'base64').toString('ascii'));
     }
 
     add(name: string, list: vscode.TextDocument[]) {
         this.groups.push({ name, list });
         const global = vscode.workspace.getConfiguration().get('tab-groups.saveGlobally', false);
-        vscode.workspace.getConfiguration().update('tab-groups.groups', this.groups, global);
+        const encoded = Buffer.from(JSON.stringify(this.groups)).toString('base64');
+        vscode.workspace.getConfiguration().update('tab-groups.groups', encoded, global);
     }
 
     remove(name: string) {
         this.groups = this.groups.filter(g => g.name !== name);
         const global = vscode.workspace.getConfiguration().get('tab-groups.saveGlobally', false);
-        vscode.workspace.getConfiguration().update('tab-groups.groups', this.groups, global);
+        const encoded = Buffer.from(JSON.stringify(this.groups)).toString('base64');
+        vscode.workspace.getConfiguration().update('tab-groups.groups', encoded, global);
     }
 
     get(name: string) {

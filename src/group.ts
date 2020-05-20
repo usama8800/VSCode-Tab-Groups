4b9d933568cb2efeeadb05d71907aaf43295a73d
Usama Ahsan <usama8800@gmail.com>
2020-05-19 21:07:59 -0400

    Convert base64 without viewcolumn

--- src/group.ts
+++ src/group.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode';
 
 interface Editor {
     document: vscode.TextDocument;
-    viewColumn: vscode.ViewColumn | undefined;
+    viewColumn?: vscode.ViewColumn;
 }
 interface Group {
     name: string;
@@ -17,6 +17,15 @@ export class Groups {
         const decoded = Buffer.from(base64, 'base64').toString('ascii');
         try { // Try to use the decoded base64
             this.groups = JSON.parse(decoded);
+            if (this.groups.length > 0 && this.groups[0].list.length > 0) {
+                const isWithoutViewColumn = (this.groups[0].list[0] as any).document === undefined;
+                if (isWithoutViewColumn) {
+                    this.groups = this.groups.map(group => ({
+                        name: group.name,
+                        list: group.list.map(list => ({ document: (list as any), viewColumn: undefined }))
+                    }));
+                }
+            }
         } catch { // Base64 decoded was not valid
             try { // Try unDecoded base64. Maybe it's saved unencoded
                 this.groups = JSON.parse(base64);
@@ -31,7 +40,13 @@ export class Groups {
     }
 
     add(name: string, editors: vscode.TextEditor[]) {
-        this.groups.push({ name, list: editors.map(editor => ({ document: editor.document, viewColumn: editor.viewColumn })) });
+        this.groups.push({
+            name,
+            list: editors.map(editor => ({
+                document: editor.document,
+                viewColumn: editor.viewColumn
+            }))
+        });
         const global = vscode.workspace.getConfiguration().get('tab-groups.saveGlobally', false);
         const encoded = Buffer.from(JSON.stringify(this.groups)).toString('base64');
         vscode.workspace.getConfiguration().update('tab-groups.groups', encoded, global);
